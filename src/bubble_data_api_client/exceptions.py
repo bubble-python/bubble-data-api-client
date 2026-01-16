11580ba7d213f9352ba91ed1c6697f2a087ffed0
purificant <purificant@users.noreply.github.com>
2026-01-16 17:17:19 +0000

    remove unused exception, we return None when there is no data

--- src/bubble_data_api_client/exceptions.py
+++ src/bubble_data_api_client/exceptions.py
@@ -13,10 +13,6 @@ class BubbleHttpError(BubbleError):
     """Base class for all high level HTTP errors."""
 
 
-class BubbleNotFoundError(BubbleHttpError):
-    """Raised when a resource is not found."""
-
-
 class BubbleUnauthorizedError(BubbleHttpError):
     """Raised when the user is not authorized to access a resource."""
 
